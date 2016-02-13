0cd341a570f45b42f36f3d93a7f9761fa0ac9089
Philip Newborough <corenominal@corenominal.org>
2016-02-13 08:21:31 +0000

    Set-up plugin

---
+++ iewp-simple-sessions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: IEWP Simple Session 
+ * Plugin URI: https://github.com/corenominal/iewp-simple-sessions
+ * Description: A WordPress plugin to provide simple PHP session management and functions.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
