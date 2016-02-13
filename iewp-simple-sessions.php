e8c3710e7b093881aeec03a4cc31b1735200a498
Philip Newborough <corenominal@corenominal.org>
2016-02-13 08:47:23 +0000

    Add session functions

--- iewp-simple-sessions.php
+++ iewp-simple-sessions.php
@@ -7,3 +7,48 @@
  * Version: 0.0.1
  * Author URI: https://corenominal.org
  */
+
+/**
+ * Start the session as early as possible
+ */
+function iewp_start_session()
+{
+	if( !session_id() )
+		session_start();
+}
+add_action( 'init', 'iewp_start_session', 1 );
+
+/**
+ * Destroy the session when a user logs out/in
+ */
+function iewp_destroy_session()
+{
+	session_destroy();
+}
+add_action( 'wp_logout', 'iewp_destroy_session' );
+add_action( 'wp_login', 'iewp_destroy_session' );
+
+/**
+ * Set a session key value pair
+ */
+if ( !function_exists( 'set_session' ) )
+{
+	function set_session( $key = 'foo', $value = 'bar' )
+	{
+		$_SESSION[$key] = $value;
+	}
+} 
+
+/**
+ * Get a session value from key
+ */
+if ( !function_exists( 'get_session' ) )
+{
+	function get_session( $key = 'foo' )
+	{
+		if( isset( $_SESSION[$key] ) )
+			return $_SESSION[$key];
+
+		return false;
+	}
+}
